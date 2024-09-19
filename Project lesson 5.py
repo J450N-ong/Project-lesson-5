6a7f0022cd9eab27820fce3d7bd7edd417a9f851
J450N-ong <odeleyejason@gmail.com>
2024-09-19 17:09:09 +0100

    Add files via upload

---
+++ Project lesson 5.py
@@ -0,0 +1,10 @@
+season1="summer"
+if season1=="summer":
+    print("Wear a light and soft cloth")
+elif season1=="winter":
+    print("Wear a jacket and pullover")
+season2="autumn"
+if season2=="spring":
+    print("Wear a top and shorts/skirts")
+elif season2=="autumn":
+    print("Wear a chunky knit cardigan")
